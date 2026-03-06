83daa9afca434986d1e63295847fcbcab21f68eb
a <a>
2026-03-05 20:36:17 -0500

    Site update..

--- generate_site.js
+++ generate_site.js
@@ -1,276 +1,165 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write
 
-/* here, we generate site */
+import { dirname } from "@std/path/dirname";
 
-const MEDIA_BASE_URL = 'https://jolinnaliarchive.github.io'
+// directory that contains this script
+const MYDIR = dirname(import.meta.url).replace(/^file:\/\//, '')
 
 Array.prototype.to_h = function() { return Object.fromEntries(this) }
 Object.prototype.to_a = function() { return Object.entries(this) }
 Array.prototype.last = function() { return this[this.length - 1] }
-
-import { dirname } from "@std/path/dirname";
-
-const MYDIR = dirname(import.meta.url).replace(/^file:\/\//, '')
-
-const inputfile = Deno.args[0]
-if (!inputfile) throw `args: <inputfile>`
-
 const assert = (cond, msg) => {
 	if (!cond) throw `Assertion failed: ${msg}`
 }
 
+if (!Deno.args[0]) {
+	throw `Please specify arg: <input file>`
+}
+
+// They are not really "pages".. more like "projects".
+// { pages }
+let { pages } = JSON.parse(await Deno.readTextFile(Deno.args[0]))
+
+for (const page of pages) {
+	page.group ??= "films"
+	page.stills = page.stills.map(x => x.replace(/^media/, '/media'))
+}
+
+// (a -> b) => [a] => { b: [a] }
+const partition = f => xs => {
+	const res = {}
+	for (const x of xs) {
+		const key = f(x)
+		res[key] ??= []
+		res[key].push(x)
+	}
+	return res
+}
+
+const projects_by_group = partition(x => x.group)(pages)
+
+const project_by_id = pages.map(x => [x.id, x]).to_h()
+
+const film_ids = `
+home-remedies
+napa
+whats-the-hurry
+series-of-experiments
+guizhou
+a-rolling-stone-gathers-no-moss
+animus
+crows-mouth-film
+home-of-rocks
+fairytale
+me-and-my-babysitter
+growing-and-leaving
+intro-film
+`.trim().split('\n')
+
+const show_ids = `
+the-womb-is-an-altar-of-all-the-things-i-miss
+the-hand-is-no-ones
+`.trim().split('\n')
+
+// console.log(film_ids.)
+const films = film_ids.map(id => project_by_id[id])
+const shows = show_ids.map(id => project_by_id[id])
+// console.error(films)
+
+const site_template = await Deno.readTextFile(`${MYDIR}/template/site.html`)
+
+const makeVideoDisplay = page => {
+	const { yt, vimeo, title_display } = page
+	if (yt && vimeo) {
+		throw `Unexpected both yt and vimeo..: ${page.id}`
+	}
+
+	let inner = ''
+	if (vimeo) {
+		// Unlisted Vimeo video seems to have two parts: video id, unlisted hash.
+		// I expect this vimeo variable to have form "videoid/unlistedhash"
+		const parts = vimeo.split('/')
+		assert(parts.length >= 1, "Vimeo no parts?")
+		inner = `<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/${parts[0]}${parts.length > 1 ? `?h=${parts[1]}` : ''}" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>`
+	} else if (yt) {
+		inner = `<iframe class=film-yt src="https://www.youtube.com/embed/${yt}" title="YouTube player for ${title_display}" frameborder=0 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+	} else {
+		console.error(page.id, "Have none")
+	}
+	return `<div class="film_video">${inner}</div>`
+}
+
 const FULLMONTHS = ['haha', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
-	.map(x => x[0].toUpperCase()+x.slice(1))
-// '2024.2' => whatever html
+	.map(s => s[0].toUpperCase() + s.slice(1))
 const date2datedisp = date => {
+	if (!date) return ''
 	const m = date.match(/(\d{4})\.(\d+)/)
 	if (!m) throw `invalid date thing: ${date}`
 	const [year, month] = m.slice(1).map(x => +x)
 	return `<time>${FULLMONTHS[month]} ${year}</time>`
 }
 
-const vimeo2vimeodisp = vimeo => {
-	if (!vimeo) return ''
-	// Unlisted Vimeo video seems to have two parts: video id, unlisted hash.
-	// I expect this vimeo variable to have form "videoid/unlistedhash"
-	const parts = vimeo.split('/')
-	assert(parts.length >= 1, "Vimeo no parts?")
-	return `<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/${parts[0]}${parts.length > 1 ? `?h=${parts[1]}` : ''}" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>`
-}
+const make_stills_slide = ({ stills }) => `
+<div class="stills_slide">
+	<div class="stills_wrapper">
+		<div class="stills">
+			${stills.map(href => `<img src="${href}" alt="still">`).join('')}
+		</div>
+	</div>
+</div>
+`
 
-const PAGEGEN =
-	{ "film": ({ title_display, yt, vimeo, md_nullable, stills, date, date_display, blocks_html }) => {
-		const yt_disp = yt
-			? `<iframe class=film-yt src="https://www.youtube.com/embed/${yt}" title="YouTube player for ${title_display}" frameborder=0 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
-			: ''
-		const vimeo_disp = vimeo2vimeodisp(vimeo)
-		const stills_disp = stills.length === 0
-			? ''
-			: `<div class=stills>${stills.map(src => `<img class=still src='${src}'>`).join('')}</div>`
-		const DISPLAY_THE_DATE = date_display ?? (date ? date2datedisp(date) : '')
-		const md = md_nullable ?? ''
+const films_content = films.map(project => {
+	const { id, stills, title_display, date, blocks_html } = project
+	return `
+<section id="${id}" class="film">
+<div class="film_description">
+	<h1>${title_display}</h1>
+	<time>${date2datedisp(date)}</time>
+	${blocks_html}
+</div>
+${makeVideoDisplay(project)}
+${make_stills_slide(project)}
+</section>
+`
+}).join('')
 
-		return `<div class=film-intro><h2 class=film-title>${title_display}</h2>`
-			+ `<div class=film-details><span>${DISPLAY_THE_DATE}</span><span>${md}</span></div>${blocks_html}</div>`
-			+ yt_disp + vimeo_disp + stills_disp
-	}
-	, "photography": ({ title_display, stills, blocks_html }) => {
-		const stills_disp = stills.length === 0
-			? ''
-			: `<div class=stills>${stills.map(src => `<img class=still src='${src}'>`).join('')}</div>`
+const shows_content = shows.map(project => {
+	const { id, stills, title_display, date, blocks_html } = project
+	return `
+<section id="${id}" class="film">
+<div class="film_description">
+	<h1>${title_display}</h1>
+	<time>${date2datedisp(date)}</time>
+	${blocks_html}
+</div>
+<div class="film_video">
+	${makeVideoDisplay(project)}
+</div>
+${make_stills_slide(project)}
+</section>
+`
+}).join('')
 
-		return `<div class=photography-intro><h2 class=photography-title>${title_display}</h2>${blocks_html}</div>`
-			+ stills_disp
-	}
-	, "writing": ({ title_display, id, date_display, writing }) => {
-		if (!writing) throw `writing has no writing: ${id}`
-		// const writing_content = await pandoc_markdown(writing)
-		return `<div class=story>
-		<h2>${title_display}</h2>
-		<span class=story-date>${date_display}</span>
-		<hr>${writing}</div>`
-	}
-	, "simple": ({title_display, blocks_html}) => `<div><h2>${title_display}</h2></div>${blocks_html}`
-	, "raw": ({ blocks_html }) => blocks_html
-	}
+const make_sidebar_content = projects => projects.map(project => {
+	const { id, stills, title_display, date, blocks_html } = project
+	return `<li><a href="#${id}">${title_display}</a></li>`
+}).join('')
 
-const page2htmlcontent = p => {
-	const gen = PAGEGEN[p.type]
+const films_sidebar_content = make_sidebar_content(films)
+const shows_sidebar_content = make_sidebar_content(shows)
 
-	if (!gen) throw `no generator for page type ${p.type} (${p.id})`
+const idToGroup = [...films, ...shows].map(({ id, group }) => [id, group]).to_h()
 
-	return gen(p)
-}
-
-// normal form for links
-const short2path = short => short === 'index'
-	? '.' // so you can host in a folder if you want
-	: `${short}`
-
-// generate nav for `curr` page
-const navstuff = ({ pages, navs }) => curr => navs.map(g => {
-
-}).filter(x => x).join('')
-
-const section2navsection = selected_page => ({ t, name, page, pages }) => {
-
-}
-
-const page2ogdescription = p => {
-	const { pagetype, filmtype } = p
-
-	if (pagetype === 'film' && filmtype == 'experimental')
-		return `An experimental film by Jolinna Li`
-	if (pagetype === 'film' && filmtype == 'narrative')
-		return `A narrative film by Jolinna Li`
-	if (pagetype === 'photography')
-		return `A series of photos taken by Jolinna Li`
-	else
-		throw `page2description error: ${JSON.stringify(p)}`
-}
-
-const page2og = p => !['film', 'photography'].includes(p.pagetype)
-	? `` :
-	`<meta property='og:title' content="${p.title_display /* NOTE: titles may have single quotes */}" />
-<meta property='og:description' content="${page2ogdescription(p)}" />
-<meta property='og:image' content="${MEDIA_BASE_URL}/${p.id}/${p.stills[0]}" />`
-
-const navthing2html = id2page => curr_page => ({ t, name, id, ids, filename }) => {
-
-	if (id) assert(id2page[id], `${id} must exist`)
-	if (ids) for(const id of ids) assert(id2page[id], `${id} must exist`)
-
-	const group_selected = id === curr_page.id || ids?.includes(curr_page.id)
-
-	if (t === 'name') {
-		return `<a short=index id=name href=.${group_selected ? ' class=current-page' : ''}>Jo Li</a>`
-	} else if (t === 'page') {
-		console.error(id, curr_page.id, group_selected)
-		return `<a short='${id}' href='${id}'${group_selected ? ' class=current-page' : ''}>${name}</a>` // alert: bad hack
-	} else if (t === 'group') {
-		return `<details group='${name}'${group_selected ? ' open' : ''}>
-			<summary>${name}</summary>
-			<ul>
-				${ids.map(id => id2page[id]).map(({ title_sidebar, id: itemid, md, stills }) => {
-					const thumbnail = md
-						? `<img class=thumb src='${stills[0]}'>`
-						: ''
-					return `<li><a short='${itemid}' class='${curr_page.id === itemid ? 'current-page ' : ''}title' href='${itemid}'>
-						${thumbnail}
-						<span group=${name}">${title_sidebar}</span>
-					</a>`
-				}).join('')}
-			</ul>
-		</details>`
-	} else {
-		throw `Unknown navthing: ${t}`
-	}
-}
-const nav2html = id2page => nav => curr_page =>
-	`<nav>${nav.map(navthing2html(id2page)(curr_page)).join('')}</nav>`
-
-// { ...page, page } => string
-const page2sitehtml = navgen => p => `<!DOCTYPE html>
-<head>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5RJJVBLRBV"></script>
+let site_html = `<!DOCTYPE html>
 <script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-5RJJVBLRBV');
+window.idToGroup = ${JSON.stringify(idToGroup)}
 </script>
-<title>${p.title_display}</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta charset="UTF-8">
-${page2og(p)}
-<link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
-<link rel=stylesheet href="./style.css">
-</head>
+` +
+	site_template
+		.replace("{{films}}", films_content).replace("{{films_sidebar}}", films_sidebar_content)
+		.replace("{{shows}}", shows_content).replace("{{shows_sidebar}}", shows_sidebar_content)
 
-<body>
-	<div id=header>${navgen(p)}</div>
-	<div id=content short=${p.id}>${p.htmlcontent}</div>
-</body>
-<script>const short_base = '${p.id}'; const path2root = '${p.path2root}'</script>
+site_html = site_html.replace(/\/?(media\/stills\/[^'"]+)/g, 'https://jolinnaliarchive.github.io/$1')
 
-${ /* scripts */
-	["d3.v7.min.js", "d3-dispatch@3.js", "d3-quadtree@3.js", "d3-timer@3.js", "d3-force@3.js", "script.js"]
-	.map(src => `<script src="./stuff/${src}"></script>`).join('')
-}
-
-`.replace(/^\t+/mg, '').replace(/$\n+/mg, ' ')
-
-const partition = pred => xs => {
-	const yes = [], no = []
-	for (const x of xs) (pred(x) ? yes : no).push(x)
-	return [yes, no]
-}
-
-const write_verbose = async (x, y) => {
-	await Deno.writeTextFile(x, y)
-	console.error(`wrote to ${x} (${y.length} chars)`)
-}
-
-const sectionify = pages => group(p => p.group)(pages)
-	.to_a()
-	.map(([grp, pages]) => {
-		if (grp === 'about') {
-			if (pages.length !== 1) throw `Must only be one About page!!`
-			return { t: 'single', name: grp, page: pages[0] }
-		} else {
-			return { t: 'group', name: grp, pages }
-		}
-	})
-
-const pages2sitejson = pages => pages
-	.map(({ htmlcontent, title_display, navgroup, id }) => [id, { page: htmlcontent, title_display, navgroup }])
-	.to_h()
-
-const { pages: _pages } = JSON.parse(await Deno.readTextFile(inputfile))
-
-import * as YAML from "jsr:@std/yaml";
-
-const nav = YAML.parse(await Deno.readTextFile(`${MYDIR}/nav.yaml`))
-
-_pages.push(
-	{ id: 'index'
-	, navgroup: 'index'
-	, type: 'raw'
-	, title_display: 'Jo Li'
-	, blocks_html: "<svg id=fun viewbox='-1000,-1000, 2000, 2000'></svg>"
-	}
-)
-
-for (const { title_display, id, date } of nav.filter(({t}) => t === 'page')) {
-	_pages.push(
-		{ id
-		, group: id
-		, navgroup: id
-		, title_display
-		, title_sidebar: title_display
-		, type: 'simple'
-		, blocks_html: await Deno.readTextFile(`${MYDIR}/${id}.html`)
-		}
-	)
-}
-
-const pages = _pages.map(_page => {
-	const { id } = _page
-	const page =
-		{ ..._page
-		, output_path: id === 'index' ? `${MYDIR}/docs/index.html` : `${MYDIR}/docs/${id}.html`
-		// , title_display: _page.title
-		}
-	page.stills = page?.stills?.map(x => `https://jolinnaliarchive.github.io/${x}`) // used by below line!
-	page.htmlcontent = page2htmlcontent(page)
-	// console.error(page.stills)
-	return page
-})
-
-const page_id_lookup = pages.map(page => [page.id, page]).to_h()
-
-for (const { id, ids, name } of nav) {
-	const allids = id ? [id] : ids ? ids : []
-	for (const id of allids) {
-		// console.error(id)
-		assert(page_id_lookup[id], `${id} must exist`)
-		page_id_lookup[id].navgroup = name
-	}
-}
-
-for (const page of pages) {
-	page.sitehtml = page2sitehtml(nav2html(page_id_lookup)(nav))(page)
-	// console.error(page.navgroup)
-}
-
-await Promise.all(pages.map(page => write_verbose(page.output_path, page.sitehtml)))
-
-// console.log(pages)
-
-
-// await Promise.all(pages.map(page =>
-// 	write_verbose(`docs/${page.output_path}`, page2sitehtml({ output_dir, sections })(page))
-// ))
-
-await write_verbose(`${MYDIR}/docs/site.json`, JSON.stringify(pages2sitejson(pages.filter(page => page.navgroup || page.id === 'index'))))
+console.log(site_html)
---
+++ template/script.js
@@ -0,0 +1,274 @@
+
+
+console.log(but_films)
+// const pages2section = {
+// 	'': 'home',
+// 	"#film-1": 'films',
+// 	"#film-2": 'films',
+// 	'#installations': 'installations',
+// 	'#bio': 'bio'
+// }
+console.log(window.pages2section)
+const pages2section = window.idToGroup ?? {}
+pages2section[''] = 'home'
+
+console.log(pages2section)
+
+
+const sections = ['home', ...[...document.querySelectorAll('#sidebar li')]
+	.map(e => {
+		console.log(e.id)
+		if (!e.id.match(/^but_\w+/)) throw `Hmm`
+		return e.id.slice(4)
+	})]
+	.map(sectionName => {
+		const content = document.querySelector(`#section_${sectionName}`)
+		const but = document.querySelector(`#but_${sectionName}`)
+		const sidebar = document.querySelector(`#sidebar_${sectionName}`)
+		pages2section[sectionName] = sectionName
+		// console.log(content)
+		return { content, but, sidebar, sectionName }
+	})
+
+let currSection = ''
+
+const showSection = sectionName => {
+	for (const section of sections) {
+		const shouldDisplay = sectionName === section.sectionName
+
+		if (section.content) section.content.hidden = !shouldDisplay
+		if (section.sidebar) section.sidebar.hidden = !shouldDisplay
+		if (section.but) section.but.style.textDecoration = shouldDisplay ? 'underline' : 'none'
+	}
+	currSection = sectionName
+}
+
+
+
+showSection(pages2section[window.location.hash?.slice(1)] ?? 'home')
+
+for (const section of sections) {
+	if (section.but) {
+		section.but.onclick = () => {
+			console.log(currSection,section.sectionName)
+			const newSection = currSection === section.sectionName ? 'home' : section.sectionName
+			showSection(newSection)
+			const hash = (currSection == 'home' ? '' : `#${currSection}`)
+			history.pushState("", document.title, window.location.pathname + window.location.search + hash)
+		}
+	}
+}
+
+
+
+const minKey = key => xs => xs.reduce((a, b) => key(a) < key(b) ? a : b)
+const maxKey = key => xs => xs.reduce((a, b) => key(a) > key(b) ? a : b)
+
+// { id: overlap ratio } => set of id
+const getOverlapping = obj =>
+	new Set(Object.entries(obj).filter(([idx, ratio]) => ratio > 0).map(([idx]) => +idx))
+
+/***** header observer ****/
+
+const setup_sidebar = (sidebar_id, content_id, section_class) => {
+	const sidebar_links = [...document.querySelectorAll(`#${sidebar_id} a`)]
+	const content_root = document.querySelector(`#${content_id}`)
+
+	const overlaps = {}
+	const section_overlaps = {}
+
+	const observer = new IntersectionObserver(entries => {
+		for (const e of entries) {
+			overlaps[e.target.myIndex] = e.intersectionRatio
+		}
+
+		const overlapping_header_idxes = getOverlapping(overlaps)
+		// const overlapping_indexes = new Set(Object.entries(overlaps).filter(([idx, ratio]) => ratio > 0).map(([idx]) => +idx))
+		const chosen_idx = overlapping_header_idxes.size === 0
+			? Math.min(...getOverlapping(section_overlaps))
+			: Math.min(...overlapping_header_idxes)
+
+		sidebar_links.forEach((e, i) => {
+			// console.log(i, overlapping_indexes, overlapping_indexes.has(i))
+			e.style.textDecoration = i === chosen_idx ? 'underline' : 'none'
+		})
+	}, {
+		root: content_root,
+		rootMargin: "0px",
+		scrollMargin: "0px",
+		threshold: [0, 0.1, 0.2, 0.3, 0.5],
+	})
+
+	// ASSUMES THERE IS ONE h1 FOR EACH THING!
+	content_root.querySelectorAll(`.${section_class} h1`).forEach((e, i) => {
+		e.myIndex = i
+		observer.observe(e)
+	})
+
+	const section_observer = new IntersectionObserver(entries => {
+		for (const e of entries) {
+			section_overlaps[e.target.myIndex] = e.intersectionRatio
+		}
+	}, {
+		root: content_root,
+		rootMargin: "0px",
+		scrollMargin: "0px",
+		threshold: [0, 0.1, 0.2, 0.3, 0.5],
+	})
+
+	document.querySelectorAll(`.${section_class}`).forEach((e, i) => {
+		e.myIndex = i
+		section_observer.observe(e)
+	})
+}
+
+setup_sidebar('sidebar_films', 'film_list_wrapper', 'film')
+// um doesnt really work..
+// setup_sidebar('sidebar_shows', 'show_list_wrapper', 'film')
+
+
+// {
+// 	const sidebar_links = [...document.querySelectorAll('#sidebar_films a')]
+
+// 	const overlaps = {}
+// 	const section_overlaps = {}
+
+// 	const observer = new IntersectionObserver(entries => {
+// 		for (const e of entries) {
+// 			overlaps[e.target.myIndex] = e.intersectionRatio
+// 		}
+
+// 		const overlapping_header_idxes = getOverlapping(overlaps)
+// 		// const overlapping_indexes = new Set(Object.entries(overlaps).filter(([idx, ratio]) => ratio > 0).map(([idx]) => +idx))
+// 		const chosen_idx = overlapping_header_idxes.size === 0
+// 			? Math.min(...getOverlapping(section_overlaps))
+// 			: Math.min(...overlapping_header_idxes)
+
+// 		sidebar_links.forEach((e, i) => {
+// 			// console.log(i, overlapping_indexes, overlapping_indexes.has(i))
+// 			e.style.textDecoration = i === chosen_idx ? 'underline' : 'none'
+// 		})
+// 	}, {
+// 		root: document.querySelector("#film_list_wrapper"),
+// 		rootMargin: "0px",
+// 		scrollMargin: "0px",
+// 		threshold: [0, 0.1, 0.2, 0.3, 0.5],
+// 	})
+
+// 	document.querySelectorAll(".film h1").forEach((e, i) => {
+// 		e.myIndex = i
+// 		observer.observe(e)
+// 	})
+
+// 	const section_observer = new IntersectionObserver(entries => {
+// 		for (const e of entries) {
+// 			section_overlaps[e.target.myIndex] = e.intersectionRatio
+// 		}
+// 	}, {
+// 		root: document.querySelector("#film_list_wrapper"),
+// 		rootMargin: "0px",
+// 		scrollMargin: "0px",
+// 		threshold: [0, 0.1, 0.2, 0.3, 0.5],
+// 	})
+
+// 	document.querySelectorAll(".film").forEach((e, i) => {
+// 		e.myIndex = i
+// 		section_observer.observe(e)
+// 	})
+// }
+
+/********************a */
+
+// const lbut_svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+
+
+const lbut_svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+lbut_svg.innerHTML = `<svg viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
+  <polygon points="0,0.5 1,0 1,1" />
+</svg>`
+
+const rbut_svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+rbut_svg.innerHTML = `<svg viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
+  <polygon points="0,0 1,0.5 0,1" />
+</svg>`
+
+document.querySelectorAll(".stills_slide").forEach(element => {
+	// console.log(e)
+	const stills = element.querySelector('.stills')
+	const imgs = stills.querySelectorAll("img")
+
+	const number_spans = [...imgs].map((e, i) => {
+		const span = document.createElement('span')
+		span.innerText = `.`
+		return span
+	})
+	const numbers = document.createElement('div')
+	numbers.className = 'stills_slide_numbers'
+	number_spans.forEach(n => numbers.appendChild(n))
+
+	const lBut = document.createElement('button')
+	// const lBut = lbut_svg.cloneNode(true)
+	const rBut = document.createElement('button')
+	lBut.className = 'stills_lbut'
+	rBut.className = 'stills_rbut'
+	lBut.appendChild(lbut_svg.cloneNode(true))
+	rBut.appendChild(rbut_svg.cloneNode(true))
+	// lBut.innerText = '<'
+	// rBut.innerText = '>'
+	stills.appendChild(lBut)
+	stills.appendChild(rBut)
+
+	element.appendChild(numbers)
+
+	// console.log(imgs)
+	const overlaps = {}
+	const observer = new IntersectionObserver(entries => {
+		for (const e of entries) {
+			overlaps[e.target.myIndex] = e.intersectionRatio
+		}
+
+		// TODO: support more than 1 in viewport at time with 100%
+
+		const overlapping_indexes = new Set(Object.entries(overlaps).filter(([idx, ratio]) => ratio > 0).map(([idx]) => +idx))
+		number_spans.forEach((e, i) => {
+			e.style.fontWeight = overlapping_indexes.has(i) ? 'bold' : 'normal'
+		})
+		// TODO: how to tell the index?
+	}, {
+		root: stills,
+		rootMargin: "0px",
+		scrollMargin: "0px",
+		threshold: [0, 0.1, 0.2, 0.3, 0.5],
+	})
+
+	imgs.forEach((e, i) => {
+		e.myIndex = i
+		observer.observe(e)
+	})
+
+	rBut.onclick = e => {
+		const overlapping_indexes = new Set(Object.entries(overlaps).filter(([idx, ratio]) => ratio > 0).map(([idx]) => +idx))
+		let rightIndex = Math.max(...overlapping_indexes)
+		if (overlapping_indexes.size === 1) rightIndex += 1
+		if (rightIndex >= imgs.length) return // at the end
+		imgs[rightIndex].scrollIntoView({block: "nearest", inline: "center"})
+
+
+	}
+	lBut.onclick = e => {
+		const overlapping_indexes = new Set(Object.entries(overlaps).filter(([idx, ratio]) => ratio > 0).map(([idx]) => +idx))
+		let leftIndex = Math.min(...overlapping_indexes)
+		if (overlapping_indexes.size === 1) leftIndex -= 1
+		if (leftIndex < 0) return // at the end
+		imgs[leftIndex].scrollIntoView({block: "nearest", inline: "center"})
+
+
+	}
+})
+
+window.onpopstate = e => {
+	if (window.location.hash) {
+		showSection(pages2section[window.location.hash?.slice(1)] ?? 'home')
+		document.querySelector(window.location.hash)?.scrollIntoView()
+	}
+}
